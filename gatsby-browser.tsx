e0f84042fad0b29ad5844182653ad4e6352d46e3
Philip Howley <howley.phil@gmail.com>
2023-03-28 18:55:55 +0100

    fix(ts): update gatsby with typescript

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { GatsbyBrowser } from 'gatsby';
 import Layout from './src/components/shared/Layout';
 
-export function wrapPageElement({ element, props }) {
+export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
+  element,
+  props,
+}) => {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Layout {...props}>{element}</Layout>;
-}
+};
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { GatsbySSR } from 'gatsby';
 import Layout from './src/components/shared/Layout';
 
-export function wrapPageElement({ element, props }) {
+export const wrapPageElement: GatsbySSR['wrapPageElement'] = ({
+  element,
+  props,
+}) => {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Layout {...props}>{element}</Layout>;
-}
+};
