c949c07bd1a605fd7c31d47562d9b1b5d7fd5d27
Philip Howley <howley.phil@gmail.com>
2023-04-29 16:24:15 +0100

    feat(ui): completed home page with Gallery component

--- src/components/atoms/images/Gallery/Gallery.tsx
+++ src/components/atoms/images/Gallery/Gallery.tsx
@@ -1,68 +1,122 @@
-// Import dependencies
-import { GatsbyImage, getImage, IGatsbyImageData } from 'gatsby-plugin-image';
+// Import required dependencies
 import React, { useEffect, useState } from 'react';
 import Masonry from 'react-masonry-css';
 import gsap from 'gsap';
+import {
+  GatsbyImage,
+  GatsbyImageProps,
+  getImage,
+  IGatsbyImageData,
+} from 'gatsby-plugin-image';
 import { useIntersectionObserver } from 'react-intersection-observer-hook';
 
-// Import styles
+// Import component styles
 import { StyledSection, StyledGallery } from './styles';
 
 // Define types for the image data and component props
+type Image = GatsbyImageProps | null | undefined;
 
-type Asset = {
-  id: string;
-  gatsbyImageData: IGatsbyImageData;
+export type GalleryProps = {
+  images: (GatsbyImageProps & {
+    imageData: IGatsbyImageData | undefined;
+    alt?: string;
+    loading?: 'auto' | 'lazy' | 'eager';
+    className?: string;
+    style?: React.CSSProperties;
+  })[];
 };
 
-type Image = {
-  id: string;
-  asset: Asset;
-  alt: string;
+type CustomImageProps = GatsbyImageProps & {
+  asset: {
+    id: string;
+    alt: string;
+    // any other properties your image object may have
+  };
 };
 
-type GalleryProps = {
-  images: Image[];
-  type?: string;
-  id?: string;
-};
+type CustomGatsbyImageProps = {
+  imageData?: IGatsbyImageData;
+  alt?: string;
+  asset: {
+    id: string;
+    alt: string;
+    // any other properties your image object may have
+  };
+} & Omit<GatsbyImageProps, 'image'> & { image: any | undefined };
 
 // Define a function to retrieve Gatsby image data from an image object
-const getGatsbyImageData = (image: Image): IGatsbyImageData | undefined => {
-  const imageData = getImage(image.asset);
-  return imageData?.images?.fallback?.src ? imageData : undefined;
+const getGatsbyImageData = (image: Image): CustomGatsbyImageProps => {
+  // Get the Gatsby image data from the provided image object
+  const imageData = getImage((image as CustomImageProps)?.asset);
+
+  // If the image object is null or undefined, return a placeholder object with default values for the component props
+  if (!image) {
+    return {
+      image: undefined,
+      imageData: imageData?.images?.fallback?.src ? imageData : undefined,
+      asset: {
+        id: (image as CustomImageProps | undefined)?.asset?.id || ``,
+        alt: (image as CustomImageProps | undefined)?.asset?.alt || ``,
+      },
+      alt: ``,
+    };
+  }
+
+  // Otherwise, return an object with the component props based on the image object and its image data
+  return {
+    image: null,
+    imageData: imageData?.images?.fallback?.src ? imageData : undefined,
+    asset: {
+      id: (image as CustomImageProps).asset.id,
+      alt: (image as CustomImageProps).asset.alt,
+    },
+    alt: image.alt || ``,
+  };
 };
 
 // Define the Gallery component
-export default function Gallery({ images }: GalleryProps) {
+const Gallery: React.FC<GalleryProps> = ({ images }) => {
   // Set up intersection observer for the component
-  const [galleryRef, { entry }] = useIntersectionObserver();
-  const [wasEverVisible, setWasEverVisible] = useState(false);
+  const [galleryRef, { entry }] = useIntersectionObserver({
+    threshold: 0.2, // The threshold for the intersection observer, which determines when the component becomes visible in the viewport
+  });
 
-  // Set breakpoints for the Masonry layout
-  const breakpoints = {
-    default: images.length >= 3 ? 3 : images.length > 1 ? 2 : 1,
-    1000: images.length >= 3 ? 3 : images.length > 1 ? 2 : 1,
-    768: images.length > 1 ? 2 : 1,
-    480: 1,
+  const [wasEverVisible, setWasEverVisible] = useState(false); // State variable that tracks whether the component has ever been visible in the viewport
+
+  // Convert each ImageWithImageData object to a CustomGatsbyImageProps object using the getGatsbyImageData function
+  const customImages: CustomGatsbyImageProps[] = images.map((image) =>
+    getGatsbyImageData(image),
+  );
+
+  // Set breakpoints for the Masonry layout, based on the number of images in the array
+
+  // Define the breakpoints for the number of images to display
+  const breakpoints: { [key: string]: number } = {
+    default: images.length >= 3 ? 3 : images.length > 1 ? 2 : 1, // The default breakpoint displays 3 images if there are 3 or more images, 2 images if there are 2, and 1 image otherwise
+    1000: images.length >= 3 ? 3 : images.length > 1 ? 2 : 1, // For screens with a width of 1000px or less, display the same number of images as the default breakpoint
+    768: images.length > 1 ? 2 : 1, // For screens with a width of 768px or less, display 2 images if there are 2 or more images, and 1 image otherwise
+    480: 1, // For screens with a width of 480px or less, display only 1 image
   };
 
   // Set up an effect to update state when the component becomes visible in the viewport
   useEffect(() => {
-    if (entry && typeof entry.isIntersecting === `boolean` && !wasEverVisible) {
-      setWasEverVisible(entry.isIntersecting);
+    if (entry?.isIntersecting === true && !wasEverVisible) {
+      // If the component is visible in the viewport and has not been visible before
+      setWasEverVisible(true); // Set wasEverVisible to true
     }
   }, [entry, wasEverVisible]);
 
   // Set up an effect to animate the images when they become visible in the viewport
   useEffect(() => {
     if (wasEverVisible) {
-      const options = [{ yPercent: -200 }, { xPercent: -200 }];
-      const randomizer = Math.floor(Math.random() * 2);
+      // If the component has ever been visible in the viewport
+      const options = [{ yPercent: -200 }, { xPercent: -200 }]; // Define the animation options
+      const randomizer = Math.floor(Math.random() * 2); // Randomly choose one of the options
       gsap.from(`.gallery .gatsby-image-wrapper`, {
-        opacity: 0,
-        stagger: 0.1,
-        ...options[randomizer],
+        // Animate the images with gsap library
+        opacity: 0, // Start with 0 opacity
+        stagger: 0.1, // Stagger the animation by 0.1s
+        ...options[randomizer], // Apply the chosen animation option
       });
     }
   }, [images, galleryRef, wasEverVisible]);
@@ -72,24 +126,28 @@ export default function Gallery({ images }: GalleryProps) {
     <StyledSection>
       <StyledGallery className="gallery" ref={galleryRef}>
         <Masonry
-          breakpointCols={breakpoints}
+          breakpointCols={breakpoints} // Set the breakpoints for the number of images to display
           className="masonry"
           columnClassName="masonry_column"
-          style={{ visibility: wasEverVisible ? `visible` : `hidden` }}
         >
-          {images.map((image) => {
-            const imageData = getGatsbyImageData(image);
-            return imageData ? (
+          {customImages.map((image, index) => (
+            <div
+              key={index}
+              className={`image-wrapper${wasEverVisible ? ` was-visible` : ``}`}
+            >
               <GatsbyImage
-                key={image.id}
-                loading="lazy"
-                image={imageData}
-                alt={image.alt}
+                image={image.imageData!}
+                alt={image.alt || ``}
+                loading={image.loading || `lazy`}
+                className={image.className || ``}
+                style={image.style || {}}
               />
-            ) : null;
-          })}
+            </div>
+          ))}
         </Masonry>
       </StyledGallery>
     </StyledSection>
   );
-}
+};
+
+export default Gallery;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-// npm
+// Import React and related modules
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
-import { PageProps, graphql, HeadProps } from 'gatsby';
-import {
-  StaticImage,
-  GatsbyImage,
-  IGatsbyImageData,
-} from 'gatsby-plugin-image';
+
+// Import Gatsby modules
+import { graphql } from 'gatsby';
+import type { PageProps, HeadProps } from 'gatsby';
+import { StaticImage, GatsbyImage } from 'gatsby-plugin-image';
+import type { IGatsbyImageData } from 'gatsby-plugin-image';
+
+// Import Font Awesome icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
 
-// components
+// Import custom components
 import HeroHeading from '../components/molecules/HeroHeading';
 import Logo from '../components/atoms/Logo';
 import Button from '../components/atoms/buttons/Button';
@@ -18,8 +20,9 @@ import SEO from '../components/shared/SEO';
 import LayoutContainer from '../components/shared/layout/LayoutContainer';
 import MediumCard from '../components/molecules/cards/MediumCard';
 import Gallery from '../components/atoms/images/Gallery';
+import type { GalleryProps } from '@/components/atoms/images/Gallery/Gallery';
 
-// styles
+// Import styled components
 import {
   StyledSection,
   PlayerWrapper,
@@ -28,6 +31,7 @@ import {
   StyledFlexContainer,
 } from './styles/HomeStyles';
 
+// Define the expected shape of the data returned by the GraphQL query
 type DataProps = {
   homePage: {
     name: string;
@@ -65,7 +69,9 @@ type DataProps = {
   };
 };
 
-const HomePage = ({ data }: PageProps<DataProps>) => {
+// Define the HomePage component
+const HomePage = ({ data: { homePage } }: PageProps<DataProps>) => {
+  // Extract the required data from the props object
   const {
     subheading,
     headingOneButtonPath,
@@ -76,15 +82,19 @@ const HomePage = ({ data }: PageProps<DataProps>) => {
     whatWeProvideBlock,
     hasGallery,
     galleryManyBlock,
-  } = data.homePage;
+  } = homePage;
 
   // const latestNewsBlog = data.latestNewsBlog.nodes;
   // const events = data.eventBlogs.nodes;
 
+  // Set up state for the video player
   const [videoOpened, setVideoOpened] = useState(false);
   const [videoError, setVideoError] = useState(false);
   const [playing, setPlaying] = useState(false);
 
+  // Define the playVideo and hideVideo event handlers
+
+  // Callback function to open the video
   const playVideo = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setVideoError(false);
@@ -92,6 +102,7 @@ const HomePage = ({ data }: PageProps<DataProps>) => {
     setPlaying(true);
   };
 
+  // Callback function to close the video
   const hideVideo = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setVideoError(false);
@@ -99,35 +110,38 @@ const HomePage = ({ data }: PageProps<DataProps>) => {
     setPlaying(false);
   };
 
-  const images = (galleryManyBlock ?? []).map((item) => ({
-    // id: item.asset.id,
-    asset: item.asset.gatsbyImageData,
-    alt: item.alt,
-  }));
-
+  // Render the HomePage component
   return (
     <>
+      {/* Render the hero section */}
       <HeroHeading home>
+        {/* Render the logo */}
         <Logo size="large" />
         <Logo size="textOnly" />
+        {/* Render the subheading */}
         <p>{subheading}</p>
+        {/* Render CTA button and video button if Vimeo ID is present */}
         <StyledFlexContainer>
           <Button actionType="link" link={headingOneButtonPath}>
             {headingOneButtonText}
           </Button>
+          {/* If the vimeoId exists and the video is not opened, render the play button */}
           {vimeoId && !videoOpened ? (
             <Button actionType="button" action={playVideo}>
               <FontAwesomeIcon icon={faPlay} /> Play {showHideVideoBtnText}
             </Button>
-          ) : null}
-          {vimeoId && videoOpened ? (
+          ) : // If the vimeoId exists and the video is opened, render the hide button
+          vimeoId && videoOpened ? (
             <Button actionType="button" action={hideVideo}>
               <FontAwesomeIcon icon={faStop} /> Hide {showHideVideoBtnText}
             </Button>
           ) : null}
         </StyledFlexContainer>
       </HeroHeading>
+
+      {/* Render the layout container */}
       <LayoutContainer hasSectionGaps>
+        {/* If vimeoId exists, render the video player */}
         {vimeoId && (
           <StyledSection
             style={{
@@ -153,14 +167,17 @@ const HomePage = ({ data }: PageProps<DataProps>) => {
             </PlayerWrapper>
           </StyledSection>
         )}
+        {/* Render error message if there's an error playing the video */}
         {vimeoId && videoOpened && videoError ? (
           <StyledSection>
             <p>There was an issue playing this video</p>
           </StyledSection>
         ) : null}
+        {/* Render the 'What We Provide' section */}
         <StyledSection>
           <StyledSectionTitle>{whatWeProvideManyHeading}</StyledSectionTitle>
           <StyledProvideCardGrid>
+            {/* Mapping over an array of objects to create cards for each item */}
             {whatWeProvideBlock.map((s) => (
               <MediumCard
                 cardType="home"
@@ -171,6 +188,7 @@ const HomePage = ({ data }: PageProps<DataProps>) => {
                 to={s.callToActionLink}
                 tagText={s.tagName}
                 image={
+                  // Displaying either a GatsbyImage or a StaticImage depending on the availability of image data
                   s?.pageImageBlock?.asset?.gatsbyImageData ? (
                     <GatsbyImage
                       image={s.pageImageBlock.asset.gatsbyImageData}
@@ -195,13 +213,27 @@ const HomePage = ({ data }: PageProps<DataProps>) => {
           </StyledProvideCardGrid>
         </StyledSection>
         <StyledSection>
-          {hasGallery ? <Gallery images={images} /> : null}
+          {hasGallery && galleryManyBlock ? ( // Conditional rendering of a Gallery component if there's a gallery available
+            <Gallery
+              images={
+                // Mapping over an array of objects to create images for the gallery
+                galleryManyBlock.map((image) => ({
+                  imageData: image.asset.gatsbyImageData,
+                  alt: image.alt,
+                  loading: `lazy`,
+                  className: `gallery-image`,
+                  style: { maxWidth: `100%` },
+                })) as GalleryProps['images']
+              }
+            />
+          ) : null}
         </StyledSection>
       </LayoutContainer>
     </>
   );
 };
 
+// the query used to retrieve data from Sanity CMS
 export const query = graphql`
   query HomePageQuery {
     homePage: sanityPage(name: { eq: "Home" }) {
@@ -253,6 +285,7 @@ export const query = graphql`
 
 export default HomePage;
 
+// a helper function Head used to generate SEO and include a script tag for SnapWidget
 export function Head({ data }: HeadProps<DataProps>) {
   const { name } = data.homePage;
   return (
