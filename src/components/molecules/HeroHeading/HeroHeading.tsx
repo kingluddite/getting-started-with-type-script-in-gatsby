73e7cb46640ed700a3ae2e83bbb1abb39e156a4d
Philip Howley <howley.phil@gmail.com>
2023-05-08 22:37:12 +0100

    feat(ui): refactor header and footer with comments

--- src/components/molecules/HeroHeading/HeroHeading.tsx
+++ src/components/molecules/HeroHeading/HeroHeading.tsx
@@ -8,21 +8,32 @@ import wing from '../../../assets/images/wing.png';
 
 interface Props {
   children?: ReactNode;
-  home?: Boolean;
-  // any props that come into the component
+  home?: boolean;
+  // Any other props that come into the component
 }
-export default function HeroHeading({ children, home = false }: Props) {
+
+/**
+ * HeroHeading component renders the heading for the Hero section
+ * @param {object} props - The props that are passed to the component
+ * @param {ReactNode} props.children - The child elements of the component
+ * @param {boolean} [props.home=false] - Flag indicating whether the component is used on the homepage
+ * @returns {JSX.Element} - The JSX markup for the component
+ */
+function HeroHeading({ children, home = false }: Props) {
   return (
     <StyledHeroHeading>
+      {/* Render the wing images on the homepage */}
       {home ? (
         <>
           <img src={wing} className="wing wing-left" alt="" />
           <img src={wing} className="wing wing-right" alt="" />
         </>
       ) : null}
+      {/* Gradient effects */}
       <div className="grad grad-right" />
       <div className="grad grad-left" />
       <div className="grad grad-top" />
+      {/* Render different styles for the homepage and other pages */}
       {home ? (
         <StyledHeroHeadingContentHome>{children}</StyledHeroHeadingContentHome>
       ) : (
@@ -32,3 +43,5 @@ export default function HeroHeading({ children, home = false }: Props) {
     </StyledHeroHeading>
   );
 }
+
+export default HeroHeading;
--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-// data
+// Import data
 import navLinkData from '../../../utils/data/nav-links';
-// styles
+import socialMediaData from '../../../utils/data/social-media';
+
+// Import styles
 import { StyledFooter } from './styles';
-// images
-// import AEFLogoLight from '../../../assets/svg/aef-lightmode-logo.svg';
-// components
+
+// Import images
+import AEFLogoLight from '../../../assets/svg/aef-lightmode-logo.svg';
+
+// Import components
 import SitemapSectionMapper from './SitemapSectionMapper';
 
 function Footer() {
   return (
     <StyledFooter>
+      {/* Render logo */}
       <div className="logo">
         <Link to="/">
-          link
-          {/* <img src={AEFLogoLight} alt="AEF Logo" /> */}
+          <img src={AEFLogoLight} alt="AEF Logo" />
         </Link>
       </div>
+
+      {/* Render copyright */}
       <div className="copyright">
         <p>copyright &copy; {new Date().getFullYear()}</p>
         <p>animation educators forum</p>
         <p>all rights reserved</p>
       </div>
+
+      {/* Render sitemap */}
       <ul className="sitemap">
+        {/* Map over nav link data to render sitemap sections */}
         {SitemapSectionMapper(`About`, navLinkData.about)}
-        {/* {sitemapSectionMapper(`Engage`, navLinkData.engage)}
-        {sitemapSectionMapper(`Programs`, navLinkData.programs)}
-        {sitemapSectionMapper(`Connect`, navLinkData.forms, socialMediaData)} */}
+        {SitemapSectionMapper(`Engage`, navLinkData.engage)}
+        {SitemapSectionMapper(`Programs`, navLinkData.programs)}
+
+        {/* Pass additional social media data to SitemapSectionMapper for "Connect" section */}
+        {SitemapSectionMapper(`Connect`, navLinkData.forms, socialMediaData)}
       </ul>
     </StyledFooter>
   );
