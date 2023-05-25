60660c4a51d5469bdaf8f554e1d8105ff08d2d1c
Philip Howley <howley.phil@gmail.com>
2023-05-25 22:29:21 +0100

    fix(ui): complete templates with ts, comments

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,13 +1,94 @@
-import type { GatsbyNode } from 'gatsby';
+import { GatsbyNode, CreatePagesArgs } from 'gatsby';
 import * as path from 'path';
 
-async function turnPastWinnersIntoPages({ graphql, actions }) {
+// Define interfaces
+interface Node {
+  [key: string]: any;
+  slug: {
+    current: string;
+  };
+}
+
+interface DataNode {
+  [key: string]: any;
+  totalCount: number;
+  nodes: Node[];
+}
+
+interface GraphQLResponse {
+  [key: string]: DataNode;
+}
+
+// Extend CreatePagesArgs interface to include graphql and actions
+interface CreatePagesParams extends CreatePagesArgs {
+  graphql: any;
+  actions: any;
+}
+
+/**
+ * Creates pages from the fetched data.
+ */
+async function createPagesFromData({
+  data,
+  actions,
+  template,
+  basePath,
+  pagePath,
+}: {
+  data: DataNode;
+  actions: CreatePagesArgs['actions'];
+  template: string;
+  basePath: string;
+  pagePath: string;
+}) {
+  if (!data || !data.nodes) {
+    throw new Error(`Failed to fetch data.`);
+  }
+
+  // Create individual pages for each node
+  data.nodes.forEach((node: Node) => {
+    actions.createPage({
+      path: `${basePath}/${node.slug.current}`,
+      component: template,
+      context: {
+        slug: node.slug.current,
+      },
+    });
+  });
+
+  // Create paginated index pages
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE || `10`, 10);
+  const pageCount = Math.ceil(data.totalCount / pageSize);
+
+  Array.from({
+    length: pageCount,
+  }).forEach((_, i) => {
+    actions.createPage({
+      path: `${pagePath}/${i + 1}`,
+      component: path.resolve(`./src/pages/${pagePath}/index.tsx`),
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
+}
+
+/**
+ * Creates pages for past winners.
+ */
+async function turnPastWinnersIntoPages({
+  graphql,
+  actions,
+}: CreatePagesParams) {
   const pastWinnersTemplate = path.resolve(
     `./src/templates/PastScholarshipWinnerTemplate/PastScholarshipWinnerTemplate.tsx`,
   );
 
   try {
-    const { data } = await graphql(`
+    // Fetch past winners data from GraphQL
+    const { data }: { data: GraphQLResponse } = await graphql(`
       query GetPastWinners {
         pastWinners: allSanityPastWinnerBlog {
           totalCount
@@ -21,50 +102,30 @@ async function turnPastWinnersIntoPages({ graphql, actions }) {
       }
     `);
 
-    if (!data || !data.pastWinners || !data.pastWinners.nodes) {
-      throw new Error(`Failed to fetch past winners data.`);
-    }
-
-    data.pastWinners.nodes.forEach((pastWinner) => {
-      actions.createPage({
-        path: `past-scholarship-winners/${pastWinner.slug.current}`,
-        component: pastWinnersTemplate,
-        context: {
-          slug: pastWinner.slug.current,
-        },
-      });
-    });
-
-    const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE || `10`);
-    const pageCount = Math.ceil(data.pastWinners.totalCount / pageSize);
-
-    Array.from({
-      length: pageCount,
-    }).forEach((_, i) => {
-      actions.createPage({
-        path: `/past-scholarship-winners/${i + 1}`,
-        component: path.resolve(
-          `./src/pages/past-scholarship-winners/index.tsx`,
-        ),
-        context: {
-          skip: i * pageSize,
-          currentPage: i + 1,
-          pageSize,
-        },
-      });
+    // Create pages from fetched data
+    await createPagesFromData({
+      data: data.pastWinners,
+      actions,
+      template: pastWinnersTemplate,
+      basePath: `past-scholarship-winners`,
+      pagePath: `past-scholarship-winners`,
     });
   } catch (error) {
     console.error(`Error creating past winners pages:`, error);
   }
 }
 
-async function turnNewsIntoPages({ graphql, actions }) {
+/**
+ * Creates pages for news.
+ */
+async function turnNewsIntoPages({ graphql, actions }: CreatePagesParams) {
   const newsTemplate = path.resolve(
     `./src/templates/NewsTemplate/NewsTemplate.tsx`,
   );
 
   try {
-    const { data } = await graphql(`
+    // Fetch news data from GraphQL
+    const { data }: { data: GraphQLResponse } = await graphql(`
       query GetNews {
         news: allSanityNews {
           totalCount
@@ -78,49 +139,30 @@ async function turnNewsIntoPages({ graphql, actions }) {
       }
     `);
 
-    if (!data || !data.news || !data.news.nodes) {
-      throw new Error(`Failed to fetch news data.`);
-    }
-
-    data.news.nodes.forEach((newsPost) => {
-      actions.createPage({
-        // url for news page
-        path: `news/${newsPost.slug.current}`,
-        component: newsTemplate,
-        context: {
-          slug: newsPost.slug.current,
-        },
-      });
-    });
-
-    const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE || `10`);
-    const pageCount = Math.ceil(data.news.totalCount / pageSize);
-
-    Array.from({
-      length: pageCount,
-    }).forEach((_, i) => {
-      actions.createPage({
-        path: `/news/${i + 1}`,
-        component: path.resolve(`./src/pages/news/index.tsx`),
-        context: {
-          skip: i * pageSize,
-          currentPage: i + 1,
-          pageSize,
-        },
-      });
+    // Create pages from fetched data
+    await createPagesFromData({
+      data: data.news,
+      actions,
+      template: newsTemplate,
+      basePath: `news`,
+      pagePath: `news`,
     });
   } catch (error) {
     console.error(`Error creating news pages:`, error);
   }
 }
 
-async function turnEventsIntoPages({ graphql, actions }) {
+/**
+ * Creates pages for events.
+ */
+async function turnEventsIntoPages({ graphql, actions }: CreatePagesParams) {
   const eventsTemplate = path.resolve(
     `./src/templates/EventTemplate/EventTemplate.tsx`,
   );
 
   try {
-    const { data } = await graphql(`
+    // Fetch events data from GraphQL
+    const { data }: { data: GraphQLResponse } = await graphql(`
       query GetEvents {
         events: allSanityEventBlog {
           totalCount
@@ -134,43 +176,23 @@ async function turnEventsIntoPages({ graphql, actions }) {
       }
     `);
 
-    if (!data || !data.events || !data.events.nodes) {
-      throw new Error(`Failed to fetch events data.`);
-    }
-
-    data.events.nodes.forEach((event) => {
-      actions.createPage({
-        path: `events/${event.slug.current}`,
-        component: eventsTemplate,
-        context: {
-          slug: event.slug.current,
-        },
-      });
-    });
-
-    const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE || `10`);
-    const pageCount = Math.ceil(data.events.totalCount / pageSize);
-
-    Array.from({
-      length: pageCount,
-    }).forEach((_, i) => {
-      actions.createPage({
-        path: `/events/${i + 1}`,
-        component: path.resolve(`./src/pages/events/index.tsx`),
-        context: {
-          skip: i * pageSize,
-          currentPage: i + 1,
-          pageSize,
-        },
-      });
+    // Create pages from fetched data
+    await createPagesFromData({
+      data: data.events,
+      actions,
+      template: eventsTemplate,
+      basePath: `events`,
+      pagePath: `events`,
     });
   } catch (error) {
     console.error(`Error creating events pages:`, error);
   }
 }
 
+// GatsbyNode's createPages function
 export const createPages: GatsbyNode['createPages'] = async (params) => {
   try {
+    // Create pages for news, events, and past winners
     await Promise.all([
       turnNewsIntoPages(params),
       turnEventsIntoPages(params),
